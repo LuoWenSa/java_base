7d6bc10b9329268fc246f3f964962cf84aa54189
luowensa <1272347203@qq.com>
2023-07-19 21:08:44 +0800

    2023.7.19 day

--- src/test/java/javaType/typeTest.java
+++ src/test/java/javaType/typeTest.java
@@ -8,8 +8,14 @@ package javaType;
 public class typeTest {
     public static void main(String[] args) {
 
-        double a = 0.6f;
-        double b = 0f;
-        System.out.println(a-b);
+        //浮点数不精确验证
+//        double a = 0.6f;
+//        double b = 0f;
+//        System.out.println(a-b);
+
+
+        Byte c = 'a';
+        System.out.println(c);
+
     }
 }
